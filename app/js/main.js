33639d56875cf62c5e6f5f3cea5e6519c8fc4fc4
Juan Mosquera <juan.mosquera@systemlab.com.pe>
2015-04-07 16:17:43 -0500

    Fix main.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,51 @@
 "use strict";
 
+var mentors_slider = $('.mentors-slider').bxSlider({
+  slideWidth: 220,
+  minSlides: 1,
+  maxSlides: 4,
+  moveSlides: 1,
+  slideMargin: 10
+});
+
+$( window ).resize(function() {
+  var window_width = $(window).width();
+  //behavior for mentors-slider
+  if(window_width>991){
+    mentors_slider.reloadSlider({
+      slideWidth: 220,
+      minSlides: 1,
+      maxSlides: 4,
+      moveSlides: 1,
+      slideMargin: 10
+    });
+  }else if(window_width>735 && window_width<=991){
+    mentors_slider.reloadSlider({
+      slideWidth: 220,
+      minSlides: 1,
+      maxSlides: 3,
+      moveSlides: 1,
+      slideMargin: 10
+    });
+  }else if(window_width>508 && window_width<=735){
+    mentors_slider.reloadSlider({
+      slideWidth: 220,
+      minSlides: 1,
+      maxSlides: 2,
+      moveSlides: 1,
+      slideMargin: 10
+    });
+  }else if(window_width<495){
+    mentors_slider.reloadSlider({
+      slideWidth: 220,
+      minSlides: 1,
+      maxSlides: 1,
+      moveSlides: 1,
+      slideMargin: 10
+    });
+  }
+});
+
 $(window).on('load',function(){
 
   $('#toggle').on('click', function(){
@@ -33,50 +79,26 @@ $(window).on('load',function(){
     }
   });
 
-  var mentors_slider = $('.mentors-slider').bxSlider({
-    slideWidth: 220,
-    minSlides: 1,
-    maxSlides: 4,
-    moveSlides: 1,
-    slideMargin: 10
-  });
+  $("#contact-submit").on('click',function() {
+    var $contact_form = $('#contact-form');
+    var fields = $contact_form.serialize();
 
-  $( window ).resize(function() {
-    var window_width = $(window).width();
-    //behavior for mentors-slider
-    if(window_width>991){
-      mentors_slider.reloadSlider({
-        slideWidth: 220,
-        minSlides: 1,
-        maxSlides: 4,
-        moveSlides: 1,
-        slideMargin: 10
-      });
-    }else if(window_width>735 && window_width<=991){
-      mentors_slider.reloadSlider({
-        slideWidth: 220,
-        minSlides: 1,
-        maxSlides: 3,
-        moveSlides: 1,
-        slideMargin: 10
-      });
-    }else if(window_width>508 && window_width<=735){
-      mentors_slider.reloadSlider({
-        slideWidth: 220,
-        minSlides: 1,
-        maxSlides: 2,
-        moveSlides: 1,
-        slideMargin: 10
-      });
-    }else if(window_width<495){
-      mentors_slider.reloadSlider({
-        slideWidth: 220,
-        minSlides: 1,
-        maxSlides: 1,
-        moveSlides: 1,
-        slideMargin: 10
-      });
-    }
+    $.ajax({
+      type: "POST",
+      url: "/php/contact.php",
+      data: fields,
+      dataType: 'json',
+      success: function(response) {
+
+        if(response.status){
+          $('#contact-form input').val('');
+          $('#contact-form textarea').val('');
+        }
+
+        $('#response').empty().html(response.html);
+      }
+    });
+    return false;
   });
 
 });
